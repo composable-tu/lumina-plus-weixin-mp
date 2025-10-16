110093cb9d60860f091c52ee949dff4c4d0dfca3
composable-tu <composable@foxmail.com>
2025-10-16 14:31:45 +0800

    chore(deps): 更新依赖项并调整构建脚本
    - 将 semver从 7.7.2 更新至 7.7.3
    - 将 @unocss/cli 和 unocss 从 66.5.2 更新至 66.5.4
    - 将 marked 从 16.3.0 更新至 16.4.0
    - 在 gulpfile.js 中使用 npm run unocss:build 替代直接命令
    -为 unocss 和 gulp 脚本添加 npx 前缀以确保正确执行环境
    - 添加 gulp-cli 作为开发依赖项

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ function generateOSSLicenses(cb) {
 }
 
 function generateUnoCSS(cb) {
-    exec('unocss src/components/**/*.wxml src/custom-tab-bar/**/*.wxml src/pages/**/*.wxml !miniprogram/pages/**/node_modules/** !miniprogram/pages/**/miniprogram_npm/** -c uno.config.js -o src/unocss.wxss', (err, stdout, stderr) => {
+    exec('npm run unocss:build', (err, stdout, stderr) => {
         if (err) {
             console.error(`生成 UnoCSS 出错: ${err}`);
             return cb(err);
